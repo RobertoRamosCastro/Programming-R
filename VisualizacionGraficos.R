64e784bb5f299a4e5736facdf5e58ba863a6f995
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-02-04 19:17:19 +0100

    Multiple graphs and functios for graphs

--- VisualizacionGraficos.R
+++ VisualizacionGraficos.R
@@ -31,8 +31,44 @@ library(ISLR)
 data(Carseats)
 str(Carseats)
 
-gg_bars <- ggplot(data=Carseats, aes(x=Sales))
-gg_bars + geom_histogram(bins=50,fill='red')
+gg_bars <- ggplot(data=Carseats, aes(x=Sales, fill=Urban))
+gg_bars + geom_histogram(position='dodge',bins=50) # bins numero de barras q salen en el histograma
+# position = 'dodge' para q no esten juntas unas con otra
+
 gg_bars + geom_bar(col='darkblue', size=10)
 
-##
+gg_bars + geom_density(alpha=0.3)
+
+#
+Carseats$Education <- as.ordered(Carseats$Education)
+ggplot(data=Carseats, aes(x=Education)) + geom_bar()
+# coord_flip() podemos rotar las coordenadas
+##FACETS
+gg_bars + geom_density(alpha=0.3) + facet_grid(Urban ~ .) #separas la leyenda
+
+# Juntando todo 
+ggplot(data=diamonds, aes(x=carat, y=price, col=color)) + 
+  geom_point()+
+  scale_x_continuous(name='Peso') +# nombre del eje X
+  scale_y_sqrt(name = 'Precio') +# nombre del eje Y y ademas escalado cuadratico
+  geom_smooth(method = 'lm', col='black') +# hacer mas smooth con el model linear model
+  scale_color_manual( # para controlar la leyenda
+    name = 'Colorcito',
+    breaks = c('D','E','F','G','H','I','J'), # lo q habia antes
+    labels = c('dd','ee','ff','gg','hh','ii','jj'), # lo q voya poner ahora a la leyenda
+    values= c('red','blue','yellow','orange','green','brown','gray')
+  )
+
+# multiples graficos
+library(gridExtra)
+g1<- ggplot(data=diamonds, aes(x= carat, y=price, col=color))
+g1_1 <- g1 + geom_point(alpha=0.4)
+g1_2 <- g1_1 + scale_color_discrete(name='Color')
+g1_2
+
+g2<- ggplot(data=diamonds, aes(x= carat, y=price, col=clarity))
+g2_1 <- g1 + geom_point(alpha=0.4)
+g2_2 <- g1_1 + scale_color_discrete(name='Color')
+g2_2
+
+grid.arrange(g1_2, g2_2, nrow=1)
