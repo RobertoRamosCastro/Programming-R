ace7f439664ab807a5b16b886c34dc22beb013bd
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-02-04 12:25:53 +0100

    Visualization data

--- VisualizacionGraficos.R
+++ VisualizacionGraficos.R
@@ -11,6 +11,17 @@ str(diamonds)
 summary(diamonds)
 
 # le pasamos data, mapping tipo aes con sus ejes X e Y para enfrentar dos variables y por ultimo su geomeotria
-ggplot(data=diamonds,mapping = aes(x=carat,y=price)) + geom_point(color='red')
+ggplot(data=diamonds,mapping = aes(x=carat,y=price, col=color)) + geom_point(alpha=1/10)# alpha es para la opacidad
 # podemos poner color tanto en aes() donde podemos usar otra variable para ponerle color o bien en la geometria pero solo colores por defecto
+# podemos tamb añadir size en vez de color con mas variables
+#dentro de la geometria podemos tocar: x,y,color,shape,fill,alpha,size
 
+##
+data("iris")
+str(iris)
+
+gg<- ggplot(data=iris, mapping=aes(x=Sepal.Length,y=Sepal.Width, col=Species)) # esto es lo de hacerlo por capas
+gg2 <- gg + geom_point()# vamos creando distintas capas y le vamos sumando cosas
+gg3 <- gg + geom_boxplot()
+gg4 <- gg + geom_contour_filled()
+gg + geom_point(size=3) + geom_line(size=0.5) 
