bc41729d765b7177b2399c513db05fa559793bac
RobertoRamosCastro <r.ramos.2017@alumnos.urjc.es>
2023-02-05 02:29:17 +0100

    Scales, facets, stats

--- Ejercicios_Visualizacion.R
+++ Ejercicios_Visualizacion.R
@@ -94,3 +94,121 @@ ggplot(diamonds, aes(x=carat, y=price, size=cut))+
 #como atributo
 ggplot(diamonds, aes(x=carat, y=price, size=cut))+
   geom_point(size=2)
+
+## Ej3
+#Disintos tipos de graficas
+data("iris")
+head(iris)
+# Una variable continua
+gg<- ggplot(iris, aes(x=Petal.Length))
+#Hist
+gg + geom_histogram()
+#Bars
+gg + geom_bar()
+#Density
+gg+geom_density()
+
+# Overplotting en scatter plots 
+data("mtcars")
+gg <- ggplot(mtcars,aes(x=as.factor(cyl), y=as.factor(am), col=as.factor(gear))) #no se porq los pone en tipo factor
+gg+
+  geom_point(size=3)
+
+gg+
+  geom_jitter(size=3)
+             
+gg+
+  geom_point(size=2, position=position_jitter(width = 0.5,height = 0.5))
+
+# Dos variables continuas
+ggplot(diamonds, aes(x=carat, y=price))+
+  geom_jitter()# con ruido, scatter es con geom_point()
+#linea
+ggplot(diamonds, aes(x=carat, y=price))+
+  geom_line()
+
+# var discreta y continua
+ggplot(diamonds, aes(x=carat, y=cut))+
+  geom_boxplot()
+ggplot(diamonds, aes(x=carat, y=cut))+
+  geom_violin() #siguiente paso al boxplot, te dice donde estan los datos y la distribucion de los mismos
+
+#Posicionamiento de las barras en un grafico de barras
+gg<-ggplot(mtcars, aes(x=as.factor(cyl), fill=as.factor(am)))
+gg+
+  geom_bar() # por defecto sale una encima de la otra pero si no, ponemos position='stack'
+gg+
+  geom_bar(position='dodge')
+
+#uno encima del otro normalizado
+gg+
+  geom_bar(position='fill')
+
+## STATS 
+ggplot(iris, aes(x=Sepal.Length,
+                 y=Sepal.Width,
+                 col=Species)) +
+  geom_point()+
+  stat_smooth()# Intenta dibujar la linea q se adapta a los puntos con su intervalo de confianza
+
+# Pintamos unicamente el stat
+ggplot(iris, aes(x=Sepal.Length,
+                 y=Sepal.Width,
+                 col=Species)) +
+  stat_smooth()
+
+# Modificamos el geom del stat: geom='line'
+ggplot(iris, aes(x=Sepal.Length,
+                 y=Sepal.Width,
+                 col=Species)) +
+  stat_smooth(geom='line') # se = F, es decir que quitemos el intervalo ese de confianaz creo 
+
+# Modificamos los aesthetics del stat: fill=Species y un valor de alpha
+ggplot(iris, aes(x=Sepal.Length,
+                 y=Sepal.Width,
+                 )) +
+  stat_smooth(aes(fill=Species), alpha=0.1)
+
+# FACETS
+# Multigraficos en un eje
+gg <- ggplot(diamonds, aes(x=carat,y=price,col=clarity))+
+  geom_point()
+gg
+
+# De una forma mucho mas clara ("desagregando" por clarity) -> facet_wrap(~clarity)
+gg + 
+  facet_wrap(~clarity) # crea un grafico por cada clarity q haya, en vez de 1 grafico con 7 puntos en la leyenda crea 7 graficas
+
+# veamos un grafico de barras de distribucion de precios por corte
+ggplot(diamonds, aes(x=price, col=cut))+geom_bar()
+
+# Mejorar la visualizacion normalizando
+ggplot(diamonds, aes(x=price, col=cut))+geom_bar(position='fill')
+
+# Como hay tantos datos, los desengranamos por cut
+ggplot(diamonds, aes(x=price, col=cut))+
+  geom_bar(position='fill')+
+  facet_wrap(~cut)
+
+# SCLAES
+gg_cont<- ggplot(diamonds,aes(x=x*y*z, y=price))+
+  geom_point()
+gg_cont
+
+gg_cont+
+  # usamos continuous porq el eje X es continua, es decir, es una variable numerica
+  scale_x_continuous(name='size',
+                     limits= c(0,800), # unicamente ver del eje X los 800 primeros
+                     breaks= seq(0,800,200), #q los 800 puntos del eje se situen cada 200
+                     minor_breaks = seq(0,800,100)) 
+
+
+# Modificcion de escala sobre variable discreta_ modifica la variable discreta segun 4 lniveles
+# I1 SI2, VVS2, IF con las etiquetas Bad Normal Good Ideal, respectivamente
+# Limita la variable "y" entre 1000 y 2000
+gg_disc<- ggplot(diamonds,aes(x=clarity, y=price))+
+  geom_jitter()
+gg_disc+
+  scale_y_continuous(limits = c(1000,2000))+
+  scale_x_discrete(limits=c("I1","SI2","VVS2","IF"),
+                   labels=c("Bad","Normal","Good","Ideal"))+
